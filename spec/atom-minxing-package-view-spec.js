4e4abcedb8ce36c158ea9b6007772426fdeeb7fe
yinhang <yinhang@dehuinet.com>
2017-11-15 16:02:03 +0800

    inital

---
+++ spec/atom-minxing-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomMinxingPackageView from '../lib/atom-minxing-package-view';
+
+describe('AtomMinxingPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
